0c52003d22f2ff45e749d3dadef78f4f6e348078
David McMackins <mcmackinsd@gmail.com>
2014-01-18 18:27:57 -0600

    Added Bukkit override methods

--- src/base/plugin/StatHandler.java
+++ src/base/plugin/StatHandler.java
@@ -1,8 +1,22 @@
 package base.plugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * @author 2mac
  */
-public class StatHandler
+public class StatHandler extends JavaPlugin
 {
+    public void onEnable()
+    {}
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        return false;
+    }
+
+    public void onDisable()
+    {}
 }
