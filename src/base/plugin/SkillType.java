f2fb7c324cb0304fc8c23f2984e380dbd9dcacd8
Jordan Giacone <Jmgiacone@gmail.com>
2014-01-19 15:01:27 -0600

    Added an enum representing different types of skills to be had.

---
+++ src/base/plugin/SkillType.java
@@ -0,0 +1,18 @@
+package base.plugin;
+
+/**
+ * @author Jdog653
+ */
+public enum SkillType
+{
+    ARCHERY,
+    AXES,
+    EXCAVATION,
+    FISHING,
+    HERBALISM,
+    MINING,
+    SWORDPLAY,
+    TAMING,
+    UNARMED_COMBAT,
+    WOODCUTTING;
+}
