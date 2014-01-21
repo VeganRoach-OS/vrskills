67c316e15f5e033b109eb2b03d5c43bad262b8f2
Jordan Giacone <Jmgiacone@gmail.com>
2014-01-20 23:18:19 -0600

    Made a HashMap to hold an array of skills for each Player. Currently resets on reload.

--- src/base/plugin/StatHandler.java
+++ src/base/plugin/StatHandler.java
@@ -1,22 +1,48 @@
 package base.plugin;
 
+import base.listeners.PlayerBlockBreakListener;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 2mac
  */
 public class StatHandler extends JavaPlugin
 {
-    public void onEnable()
-    {}
+    public Map<String, Skill[]> playerSkills;
 
+    @Override
+    public void onEnable()
+    {
+        Skill[] s;
+        playerSkills = new HashMap<>();
+
+        for(Player p : Bukkit.getOnlinePlayers())
+        {
+            s = new Skill[10];
+
+            for(int i = 0; i < 10; i++)
+            {
+                s[i] = new Skill(SkillType.values()[i]);
+            }
+
+            playerSkills.put(p.getDisplayName(), s);
+        }
+    }
+
+    @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
         return false;
     }
 
+    @Override
     public void onDisable()
     {}
 }
