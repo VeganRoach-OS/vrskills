824b52f00ebcf52f963ae1ee759ab559b0beb70f
Jordan Giacone <Jmgiacone@gmail.com>
2014-01-23 22:39:10 -0600

    Made an enum to represent the various config files Forgot to add this file

---
+++ src/base/ConfigFile.java
@@ -0,0 +1,21 @@
+package base;
+
+/**
+ * Created by Jordan on 1/23/14.
+ */
+public enum ConfigFile
+{
+    EXPERIENCE("experience.yml");
+
+    private final String FILE;
+
+    ConfigFile(final String s)
+    {
+        FILE = s;
+    }
+
+    public String getFile()
+    {
+        return FILE;
+    }
+}
