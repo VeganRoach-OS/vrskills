02c9a29fbc352e1ee9cb5312d31eea47005ae2dc
David McMackins <mcmackinsd@gmail.com>
2014-01-30 08:05:38 -0600

    Changed experience gain process

--- src/base/listeners/PlayerBlockBreakListener.java
+++ src/base/listeners/PlayerBlockBreakListener.java
@@ -26,21 +26,32 @@ public class PlayerBlockBreakListener implements Listener
     public void onBlockBreak(BlockBreakEvent e)
     {
         Player player = e.getPlayer();
-        int[] expAmounts = new int[SkillType.values().length];
+        int expAmount;
+        String skill = "";
 
         for (int i = 0; i < SkillType.values().length; i++)
         {
-            expAmounts[i] = plugin.experience.getInt(SkillType.values()[i] + ".blocks." + e.getBlock().getType());
+            if (plugin.experience.getInt(SkillType.values()[i].toString() + ".blocks." + e.getBlock().getType()) > 0)
+            {
+                skill = SkillType.values()[i].toString();
+                break;
+            }
         }
 
+        expAmount = plugin.experience.getInt(skill + ".blocks." + e.getBlock().getType());
+
         for (int i = 0; i < SkillType.values().length; i++)
         {
-            if (plugin.playerSkills.get(player.getName())[i].addExp(expAmounts[i]))
-                player.sendMessage(ChatColor.DARK_RED + "Congratulations! You've just advanced your skill in " +
-                                    ChatColor.YELLOW + plugin.playerSkills.get(player.getName())[i].getType() +
-                                    ChatColor.DARK_RED + "! (" +
-                                    ChatColor.YELLOW + plugin.playerSkills.get(player.getName())[i].getCurrentLevel() +
-                                    ChatColor.DARK_RED + ")");
+            if (plugin.playerSkills.get(player.getName())[i].getType().toString().equalsIgnoreCase(skill))
+                if (plugin.playerSkills.get(player.getName())[i].addExp(expAmount))
+                {
+                    player.sendMessage(ChatColor.DARK_RED + "Congratulations! You've just advanced your skill in " +
+                                               ChatColor.YELLOW + plugin.playerSkills.get(player.getName())[i].getType() +
+                                               ChatColor.DARK_RED + "! (" +
+                                               ChatColor.YELLOW + plugin.playerSkills.get(player.getName())[i].getCurrentLevel() +
+                                               ChatColor.DARK_RED + ")");
+                    break;
+                }
         }
     }
 }
